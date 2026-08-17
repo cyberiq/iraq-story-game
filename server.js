8f76d1ece119c8b9d9d25f4c87225ff97b91d502
kali <kali@local>
2026-08-17 03:10:50 +0300

    update

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ app.use(
     }
   })
 );
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), { index: false }));
+app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
 
 function requireAdmin(req, res, next) {
   if (req.session && req.session.isAdmin === true) {
@@ -607,6 +608,10 @@ app.use((error, req, res, next) => {
   return next(error);
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login.html"));
 });
@@ -628,7 +633,11 @@ app.use((req, res, next) => {
     return res.status(404).json({ error: "API route not found" });
   }
 
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  if (req.path.includes(".")) {
+    return res.status(404).send("Not Found");
+  }
+
+  return res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 (async () => {
